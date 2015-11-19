e3b179f3dfb36b7833a225f3b107b1bd66ca8a27
cjh5414 <cjh5414@gmail.com>
2015-11-19 11:55:19 +0900

    intent 수정

--- intent/src/main/java/me/dong/intent/CalcActivity.java
+++ intent/src/main/java/me/dong/intent/CalcActivity.java
@@ -3,6 +3,8 @@ package me.dong.intent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 /**
@@ -14,28 +16,33 @@ public class CalcActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calc);
 
-        double result=0.0;
-        Intent inIntent = getIntent();
+        Button btnResult = (Button) findViewById(R.id.btnResult);
+        btnResult.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                double result = 0.0;
+                Intent inIntent = getIntent();
 
-        switch(inIntent.getStringExtra("Calc")) {
-            case "plus":
-                result = inIntent.getDoubleExtra("Num1", 0) + inIntent.getDoubleExtra("Num2", 0);
-                break;
-            case "minus":
-                result = inIntent.getDoubleExtra("Num1", 0) - inIntent.getDoubleExtra("Num2", 0);
-                break;
-            case "mul":
-                result = inIntent.getDoubleExtra("Num1", 0) * inIntent.getDoubleExtra("Num2", 0);
-                break;
-            case "div":
-                result = inIntent.getDoubleExtra("Num1", 0) / inIntent.getDoubleExtra("Num2", 0);
-                break;
-        }
+                switch (inIntent.getStringExtra("Calc")) {
+                    case "plus":
+                        result = inIntent.getDoubleExtra("Num1", 0) + inIntent.getDoubleExtra("Num2", 0);
+                        break;
+                    case "minus":
+                        result = inIntent.getDoubleExtra("Num1", 0) - inIntent.getDoubleExtra("Num2", 0);
+                        break;
+                    case "mul":
+                        result = inIntent.getDoubleExtra("Num1", 0) * inIntent.getDoubleExtra("Num2", 0);
+                        break;
+                    case "div":
+                        result = inIntent.getDoubleExtra("Num1", 0) / inIntent.getDoubleExtra("Num2", 0);
+                        break;
+                }
 
-        Intent outIntent = new Intent(getApplicationContext(), MainActivity.class);
-        outIntent.putExtra("Result", result);
+                Intent outIntent = new Intent(getApplicationContext(), MainActivity.class);
+                outIntent.putExtra("Result", result);
 
-        setResult(RESULT_OK, outIntent);
-        finish();
+                setResult(RESULT_OK, outIntent);
+                finish();
+            }
+        });
     }
 }
